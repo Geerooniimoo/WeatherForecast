cc3acf0cb819c363c0dac5928cfa6e1a4d8e40c9
Geronimo Perez <geronimo.perez4@gmail.com>
2023-02-25 11:13:36 -0500

    Free key is visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,11 @@
-const getKey = () => {
-    if (!localStorage.apiKey || localStorage.apiKey == 'null') {
-        localStorage.apiKey = prompt("Please enter Open Weather Map API Key.")
-        getKey();
-    };
-};
-
-getKey();
-
-let store = [];
+let store = localStorage.cities ? JSON.parse(localStorage.cities) : [];
 
 const renderStore = () => {
-    if (localStorage.cities) {
-        store = eval(localStorage.cities);
-
+    if (store.length) {
         $(".list-group").empty();
 
         store.forEach(city => {
-            $(".list-group").prepend($(`<button onclick="forecast('${city}')">${city}</button>`));
+            $(".list-group").append($(`<div><button onclick="forecast('${city}')">${city}</button><span class="del">&#10060;</span></div>`));
         });
     };
 };
@@ -25,52 +14,45 @@ renderStore();
 
 const forecast = async loc => {
 
-    if (!loc) return;
+    if (!loc && !store.length) return;
+    if (!loc && store.length) loc = store[0];
 
-    let url1 = `https://api.openweathermap.org/geo/1.0/direct?q=${loc}&APPID=${localStorage.apiKey}&limit=1`;
-    let data = await fetch(url1).then(data => data.json());
-    x = data;
-    let [{lat,lon,name:city,country}] = await fetch(url1).then(data => data.json());
-    let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&APPID=${localStorage.apiKey}&units=imperial`;
-    
-    let {
-            daily,current:{dt,humidity,temp,uvi,weather:[{description,icon}]}
-        } = await fetch(url2).then(data => data.json());
-        // let data = await fetch(url2).then(data => data.json());
+    let url1 = `https://api.openweathermap.org/geo/1.0/direct?q=${loc}&APPID=${apiKey}&limit=1`;
+    let [{ lat, lon, name: city, country }] = await (await fetch(url1)).json();
 
-        // x = daily;
-    
-    if(!store.includes(city)) {
-        store.push(city);
-        localStorage.cities = JSON.stringify(store);
-        renderStore();
-    };
+    let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&APPID=${apiKey}&units=imperial`;
+    let { daily, current: { dt, humidity, temp, uvi, weather: [{ description, icon }] } } = await (await fetch(url2)).json();
+
+    store = store.filter(obj => obj != city);
+    store = [city, ...store];
+    localStorage.cities = JSON.stringify(store);
+    renderStore();
 
     let $div = `<div class="ml-4">
                     <h1 class="display-3 text-center">${city}, ${country}</h1>
                     <hr>
-                    <h2>${moment(dt*1000).format('llll')}</h2>
+                    <h2>${moment(dt * 1000).format('llll')}</h2>
                     <h2>Temperature: ${temp} °F</h2>
                     <h2>Humidity: ${humidity}</h2>
                     <h2>Description: ${description}</h2>
-                    <h2>UV Index:<span class="uvi ${uvi<3?'green':uvi<6?'yellow':uvi<8?'orange':uvi<11?'red':'purple'}"> ${uvi} </span></h2>
+                    <h2>UV Index:<span class="uvi ${uvi < 3 ? 'green' : uvi < 6 ? 'yellow' : uvi < 8 ? 'orange' : uvi < 11 ? 'red' : 'purple'}"> ${uvi} </span></h2>
                     <img class="mainIcon" src="http://openweathermap.org/img/w/${icon}.png">
                 </div>`
-  
+
     $(".jumbotron").html($div);
 
     $(".card-deck").empty();
 
     for (let i = 0; i < 5; i++) {
-        let { dt, humidity, uvi, temp:{max:temp}, weather:[{description,icon}] } = daily[i];
-        
+        let { dt, humidity, uvi, temp: { max: temp }, weather: [{ description, icon }] } = daily[i];
+
         const $card = `<div class="card text-white">
             <div class="card-body p-0">
-                <p class="card-text day"> ${moment(dt*1000).format('dddd')} </p>
+                <p class="card-text day"> ${moment(dt * 1000).format('dddd')} </p>
                 <p class="card-text">Temp: ${temp} °F</p>
                 <p class="card-text">Hum: ${humidity} </p>
                 <p class="card-text"> ${description} </p>
-                <p class="card-text">UVi: <span class="uvi ${uvi<3?'green':uvi<6?'yellow':uvi<8?'orange':uvi<11?'red':'purple'}">${uvi}</span></p>
+                <p class="card-text">UVi: <span class="uvi ${uvi < 3 ? 'green' : uvi < 6 ? 'yellow' : uvi < 8 ? 'orange' : uvi < 11 ? 'red' : 'purple'}">${uvi}</span></p>
                 <img class="m-auto" src="http://openweathermap.org/img/w/${icon}.png">
                 </div>
                 </div>`
@@ -78,5 +60,7 @@ const forecast = async loc => {
         $(".card-deck").append($card);
     }
 };
+
+forecast();
 
 $('.submit').on('click', () => forecast($('input').val()));
