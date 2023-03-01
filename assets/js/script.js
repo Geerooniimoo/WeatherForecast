35083f482219b57f7271020f921668a0dbee5531
Geronimo Perez <geronimo.perez4@gmail.com>
2023-03-01 11:18:30 -0500

    rearranging order in mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,25 +21,28 @@ const forecast = async loc => {
     let [{ lat, lon, name: city, country }] = await (await fetch(url1)).json();
 
     let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&APPID=${apiKey}&units=imperial`;
-    let { daily, current: { dt, humidity, temp, uvi, weather: [{ description, icon }] } } = await (await fetch(url2)).json();
 
+    let data = await (await fetch(url2)).json();
+    x=data;
+    let { daily, current: { dt, humidity, temp, uvi, weather: [{ description, icon }] } } = data;
+    
     store = store.filter(obj => obj != city);
     store = [city, ...store];
     localStorage.cities = JSON.stringify(store);
     renderStore();
 
-    let $div = `<div class="ml-4">
-                    <h1 class="display-3 text-center">${city}, ${country}</h1>
-                    <hr>
+    currentDiv.innerHTML = `<div class="ml-4">
+                    <h1>${city}, ${country}</h1>
                     <h2>${moment(dt * 1000).format('llll')}</h2>
+                    <hr>
                     <h2>Temperature: ${temp} °F</h2>
-                    <h2>Humidity: ${humidity}</h2>
                     <h2>Description: ${description}</h2>
+                    <h2>Humidity: ${humidity}</h2>
                     <h2>UV Index:<span class="uvi ${uvi < 3 ? 'green' : uvi < 6 ? 'yellow' : uvi < 8 ? 'orange' : uvi < 11 ? 'red' : 'purple'}"> ${uvi} </span></h2>
                     <img class="mainIcon" src="http://openweathermap.org/img/w/${icon}.png">
                 </div>`
 
-    $(".jumbotron").html($div);
+    // $(".jumbotron").html($div);
 
     $(".card-deck").empty();
 
@@ -50,17 +53,17 @@ const forecast = async loc => {
             <div class="card-body p-0">
                 <p class="card-text day"> ${moment(dt * 1000).format('dddd')} </p>
                 <p class="card-text">Temp: ${temp} °F</p>
-                <p class="card-text">Hum: ${humidity} </p>
                 <p class="card-text"> ${description} </p>
+                <p class="card-text">Hum: ${humidity} </p>
                 <p class="card-text">UVi: <span class="uvi ${uvi < 3 ? 'green' : uvi < 6 ? 'yellow' : uvi < 8 ? 'orange' : uvi < 11 ? 'red' : 'purple'}">${uvi}</span></p>
                 <img class="m-auto" src="http://openweathermap.org/img/w/${icon}.png">
                 </div>
-                </div>`
+            </div>`
 
         $(".card-deck").append($card);
     }
 };
 
-forecast();
+// forecast();
 
-$('.submit').on('click', () => forecast($('input').val()));
+$('#submit').on('click', () => forecast($('input').val()));
